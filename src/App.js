264d9432e8f0c528b6d0115cab8ae1f757aa13d2
Francisco Castillo <cas21562@uvg.edu.gt>
2023-06-06 23:18:42 -0600

    Session Navigation Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import './App.scss';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {supabase} from './supabase/client.js';
 import SignUp from "./pages/SignUp/SignUp";
 import LogIn from "./pages/LogIn/LogIn";
 import Home from "./pages/Home/Home";
 
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const {data: authListener} = supabase.auth.onAuthStateChange(
+            (event, session) => {
+                if (event === 'SIGNED_IN') {
+                    setIsLoggedIn(true);
+                } else if (event === 'SIGNED_OUT') {
+                    setIsLoggedIn(false);
+                }
+            }
+        );
+
+        return () => {
+            authListener.subscription.unsubscribe();
+        };
+    }, []);
+
     return (
-        <div className={"app"}>
+        <div className="App">
             <Router>
-                <Routes>
-                    <Route path="/" element={<SignUp/>}/>
-                    <Route path="/signIn" element={<LogIn/>}/>
-                    <Route path="/signUp" element={<SignUp/>}/>
-                    <Route path="/home" element={<Home/>}/>
-                </Routes>
+                {isLoggedIn
+                    ?
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/home" element={<Home/>}/>
+                    </Routes>
+                    :
+                    <Routes>
+                        <Route path="/" element={<LogIn/>}/>
+                        <Route path="/signup" element={<SignUp/>}/>
+                    </Routes>
+                }
             </Router>
         </div>
-    );
+    )
 }
 
 export default App;
