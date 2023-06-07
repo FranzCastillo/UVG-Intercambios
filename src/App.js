efeb202bbb150cf6160803961c5c7c83de862927
Francisco Castillo <cas21562@uvg.edu.gt>
2023-06-06 20:37:17 -0600

    Styles Set

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import SignUp from "./pages/SignUp/SignUp";
+import LogIn from "./pages/LogIn/LogIn";
+
+const App = () => {
+    return (
+        <div className={"app"}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LogIn/>}/>
+                    <Route path="/signIn" element={<LogIn/>}/>
+                    <Route path="/signUp" element={<SignUp/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
