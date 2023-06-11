6ec9c7bc5273524996eaefd5359f1944ad6e196f
Francisco Castillo <cas21562@uvg.edu.gt>
2023-06-10 22:54:30 -0600

    Universities Buttons Navigation Implemented

--- src/pages/Universities/Universities.js
+++ src/pages/Universities/Universities.js
@@ -1,27 +1,42 @@
 import Container from "@mui/material/Container";
 import {Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
-import "./Universities.scss"
-import Table from './Table/Table'
+import "./Universities.scss";
+import Table from './Table/Table';
+import Button from "@mui/material/Button";
+import DomainAddIcon from '@mui/icons-material/DomainAdd';
+import {useNavigate} from "react-router-dom";
 
 const Universities = () => {
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        navigate("/universities/new");
+    }
+
     return (
-        <Container
-            maxWidth={"xl"}
-            className={"students"}
-        >
-            <Paper className={"container"}>
-                <Typography
-                    variant={"h4"}
-                    className={"title"}
-                    sx={{fontFamily: "Roboto"}}
-                >
+        <Container maxWidth="xl" className="students">
+            <Paper className="container">
+                <Typography variant="h4" className="title" sx={{fontFamily: "Roboto"}}>
                     Universidades
                 </Typography>
+                <Button
+                    variant="contained"
+                    color="success"
+                    sx={{
+                        width: "150px",
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}
+                    onClick={handleRegister}
+                >
+                    <DomainAddIcon/>
+                    Registrar
+                </Button>
             </Paper>
-            <Table />
+            <Table/>
         </Container>
-    )
-}
+    );
+};
 
-export default Universities
+export default Universities;
